2ee8423c3dd7af3e5a31cd87aae1d64ceab47840
MaxLeblc <maxime.leb@gmail.com>
2026-08-08 16:46:29 +0200

    feat: setup websocket stomp configuration and chat controller

---
+++ backend/chatPOC/src/main/java/com/yourCarYourWay/chatPOC/model/ChatMessage.java
@@ -0,0 +1,26 @@
+package com.yourCarYourWay.chatPOC.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String sender;
+    private String content;
+    private MessageType type;
+    private LocalDateTime timestamp;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
